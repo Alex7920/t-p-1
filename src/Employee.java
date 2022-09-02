a1bfb13e4668e0aeebad6b6031bead58c33a0fdd
Александр <kuzya-79@yandex.ru>
2022-09-02 11:27:40 +0300

    КУРСОВАЯ РАБОТА 1

--- src/Employee.java
+++ src/Employee.java
@@ -1,65 +1,44 @@
 public class Employee {
 
-    private static int countter;
+    private String FIO;
+    private int Department;
+    private float Salary;
+    private static int Id;
 
-    private Integer id;
-    private String name;
-    private String LastName;
-    private String middleName;
-    private int department;
-    private int salary;
 
-    public Employee(String name, String LastName, String middleName, int department, int salary) {
-        this.name = name;
-        this.LastName = LastName;
-        this.middleName = middleName;
-        this.department = department;
-        this.salary = salary;
-        this.id = countter++;
+    public int getId() { return this.Id; }
+    public String getFIO() { return this.FIO; }
+    public int getDepartment() { return this.Department; }
+    public float getSalary() { return this.Salary; }
 
-    }
 
-    public String getName() {
-        return name;
-    }
+    public void SetFIO(String fio) { this.FIO = fio; }
+    public void SetDepartment(int dept) { this.Department = dept; }
+    public void SetSalary(float sal) { this.Salary = sal; }
 
-    public String getLestName() {
-        return LastName;
-    }
 
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public int getDepartment() {
-            return department;
-    }
-
-    public Integer getId() {
-        return this. id;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setDepartment(byte department) {
-        this.department = department;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
+    public Employee(String fio, int dept, float salary) {
+        FIO = fio;
+        Department = dept;
+        Salary = salary;
+        Id++;
     }
 
     @Override
     public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", LastName='" + LastName + '\'' +
-                ", middleName='" + middleName + '\'' +
-                ", department=" + department +
-                ", salary=" + salary +
-                '}';
+        return "Fio: " + FIO + " Dept: " + Department + " Salary: " + Salary;
+    }
+
+
+    public static void main(String[] args) {
+        Employee[] empl = new Employee[3];
+        empl[1] = new Employee("Fam1 Name1 SName1", 1, 25500f);
+        empl[2] = new Employee("Fam2 Name2 SName2", 2, 31450f);
+        empl[3] = new Employee("Fam3 Name3 SName3", 3, 40200f);
+
+
+        for (Employee e: empl) {
+            System.out.println(e);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,36 @@
 public class Main {
-    private static final Employee[] employees = new Employee[10];
+    private static final Employee[] EMPLOYEES = new Employee[10];
 
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("Иван", "Иванович", "Ивонов", 1, 11200);
-        Employee emp2 = new Employee("Иван", "Иванович", "Ивонов", 1, 11200);
+        Employee emp1 = new Employee("Fam1 Name1 SName1", 1, 25500f);
+        Employee emp2 = new Employee("Fam2 Name2 SName2", 2, 31450f);
+        Employee emp3 = new Employee("Fam3 Name3 SName3", 3, 40200f);
         System.out.println(emp1);
         System.out.println(emp2);
-        employees[0] = emp1;
+        System.out.println(emp3);
+        EMPLOYEES[0] = emp1;
 
     }
     public static void printEmployees() {
-        for (Employee employee : employees) {
+        for (Employee employee : EMPLOYEES) {
             System.out.println(employee);
         }
     }
     public static int calculateTotalSalary() {
         int sum = 0;
-        for (Employee employee : employees) {
+        for (Employee employee : EMPLOYEES) {
             sum += employee.getSalary();
         }
         return sum;
 
     }
     public static Employee findEmployeeWithMinSalary() {
-       Employee result = employees[0];
-       int minSalary = employees[0].getSalary();
-       for (Employee employee : employees) {
+       Employee result = EMPLOYEES[0];
+       int minSalary = (int) EMPLOYEES[0].getSalary();
+       for (Employee employee : EMPLOYEES) {
            if (employee.getSalary() < minSalary) {
-               minSalary = employee.getSalary();
+               minSalary = (int) employee.getSalary();
                result = employee;
            }
        }
