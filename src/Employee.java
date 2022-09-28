e2822418188976e5fe3eda21aff155446cfc1e9c
Александр <kuzya-79@yandex.ru>
2022-09-28 11:04:38 +0300

    КУРСОВАЯ РАБОТА 1 исправлено

--- src/Employee.java
+++ src/Employee.java
@@ -1,44 +1,60 @@
 public class Employee {
 
-    private String FIO;
-    private int Department;
-    private float Salary;
-    private static int Id;
+    private int id;
+    private String fio;
+    private int department;
+    private double salary;
+    private static int counter;
 
 
-    public int getId() { return this.Id; }
-    public String getFIO() { return this.FIO; }
-    public int getDepartment() { return this.Department; }
-    public float getSalary() { return this.Salary; }
+    public Employee(String fio, int department, double salary) {
+        this.id = ++counter;
+        this.fio = fio;
+        this.department = department;
+        this.salary = salary;
+    }
 
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
 
-    public void SetFIO(String fio) { this.FIO = fio; }
-    public void SetDepartment(int dept) { this.Department = dept; }
-    public void SetSalary(float sal) { this.Salary = sal; }
+    public void setDepartment(int department) {
+        if (department < 0 || department > 5) throw new IllegalArgumentException("Введено невалидное значение");
+        this.department = department;
+    }
 
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
-    public Employee(String fio, int dept, float salary) {
-        FIO = fio;
-        Department = dept;
-        Salary = salary;
-        Id++;
+    public int getId() {
+        return id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public static int getCounter() {
+        return counter;
     }
 
     @Override
     public String toString() {
-        return "Fio: " + FIO + " Dept: " + Department + " Salary: " + Salary;
+        return "Employee{" +
+                "id=" + id +
+                ", fio='" + fio + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
     }
 
-
-    public static void main(String[] args) {
-        Employee[] empl = new Employee[3];
-        empl[1] = new Employee("Fam1 Name1 SName1", 1, 25500f);
-        empl[2] = new Employee("Fam2 Name2 SName2", 2, 31450f);
-        empl[3] = new Employee("Fam3 Name3 SName3", 3, 40200f);
-
-
-        for (Employee e: empl) {
-            System.out.println(e);
-        }
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,97 @@
 public class Main {
-    private static final Employee[] EMPLOYEES = new Employee[10];
+    private static Employee[] employees = new Employee[10];
 
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("Fam1 Name1 SName1", 1, 25500f);
-        Employee emp2 = new Employee("Fam2 Name2 SName2", 2, 31450f);
-        Employee emp3 = new Employee("Fam3 Name3 SName3", 3, 40200f);
-        System.out.println(emp1);
-        System.out.println(emp2);
-        System.out.println(emp3);
-        EMPLOYEES[0] = emp1;
+        employees[0] = null;
+        employees[1] = new Employee("Fam1 Name1 SName1", 1, 20000);
+        employees[2] = new Employee("Fam2 Name2 SName2", 2, 35050);
+        employees[3] = new Employee("Fam3 Name3 SName3", 3, 40200);
+        printAllEmployee();
+        System.out.println(getCalculateSumSalary());
+        System.out.println(getEmployeeMinSalary());
+        System.out.println(getEmployeeMaxSalary());
+        System.out.println(getAverageSalaryEmployee());
+        printNameAllEmployee();
 
     }
-    public static void printEmployees() {
-        for (Employee employee : EMPLOYEES) {
+
+    public static void printAllEmployee() {
+        for (Employee employee : employees) {
             System.out.println(employee);
         }
     }
-    public static int calculateTotalSalary() {
-        int sum = 0;
-        for (Employee employee : EMPLOYEES) {
+
+    public static double getCalculateSumSalary() {
+        double sum = 0;
+        for (Employee employee : employees) {
+            if (employee == null) continue;
             sum += employee.getSalary();
         }
         return sum;
-
     }
-    public static Employee findEmployeeWithMinSalary() {
-       Employee result = EMPLOYEES[0];
-       int minSalary = (int) EMPLOYEES[0].getSalary();
-       for (Employee employee : EMPLOYEES) {
-           if (employee.getSalary() < minSalary) {
-               minSalary = (int) employee.getSalary();
-               result = employee;
-           }
-       }
-       return result;
+
+    public static Employee getEmployeeMinSalary() {
+        double min = 0;
+        int index = 0;
+        Employee employeeMinSalary = null;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                min = employees[i].getSalary();
+                employeeMinSalary = employees[i];
+                index = i;
+                break;
+            }
+        }
+
+        for (int i = index; i < employees.length; i++) {
+            if (employees[i] == null) continue;
+            if (employees[i].getSalary() < min) {
+                min = employees[i].getSalary();
+                employeeMinSalary = employees[i];
+            }
+        }
+        return employeeMinSalary;
+    }
+
+    public static Employee getEmployeeMaxSalary() {
+        double max = 0;
+        int index = 0;
+        Employee employeeMaxSalary = null;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                max = employees[i].getSalary();
+                employeeMaxSalary = employees[i];
+                index = i;
+                break;
+            }
+        }
+        for (int i = index; i < employees.length; i++) {
+            if (employees[i] == null) continue;
+            if (employees[i].getSalary() > max) {
+                max = employees[i].getSalary();
+                employeeMaxSalary = employees[i];
+            }
+        }
+        return employeeMaxSalary;
+    }
+
+    public static double getAverageSalaryEmployee() {
+        double sum = 0;
+        int countEmp = 0;
+        for (Employee emp : employees) {
+            if (emp == null) continue;
+            countEmp++;
+            sum += emp.getSalary();
+        }
+        return sum/countEmp;
+    }
+    public static void printNameAllEmployee(){
+      for (Employee empl : employees) {
+          if (empl == null) continue;
+          System.out.println(empl.getFio());
+      }
     }
 }
+
+
